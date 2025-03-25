6ebbbec1f319b52c6bfb15b7136ebfdb30c0e86f
Kris Thompson <kris@austinvoicedata.com>
2025-03-25 21:34:17 +0000

    oops

--- place_views.py
+++
@@ -1,9 +0,0 @@
-from django.db.models import QuerySet
-from .models import Place
-from .views_fun import get_annotated_places
-
-class PlaceListView:
-    def get_queryset(self) -> QuerySet[Place]:
-        if not hasattr(self, '_queryset'):
-            self._queryset = get_annotated_places()  # Using a function from views_fun.py
-        return self._queryset 
